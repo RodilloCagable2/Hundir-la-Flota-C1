f757b4782babedbaa671826abff14bad580940a2
Adrián <127325984+RodilloCagable2@users.noreply.github.com>
2025-03-09 20:33:29 +0100

    Add files via upload

---
+++ Complementos.h
@@ -0,0 +1,24 @@
+#ifndef COMPLEMENTOS_H_
+#define COMPLEMENTOS_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+void clear ();
+void titulo ();
+
+//PRECONDICIÓN: Se le pasa una cadena
+//POSCONDICIÓN: Se le cambia el caracter salto de linea por terminador
+void terminador_cad (char []);
+
+//PRECONDICIÓN: Ninguna
+//POSCONDICIÓN: Devuelve un caracter para respuestas si/no
+char confirmacon ();
+
+//PRECONDICIÓN: Ninguna
+//POSCONDICIÓN: Devuelve siempre un número entero
+int input_int ();
+
+#endif
