7974194161cbc18916b4c691bb633cfb3be76b39
Lucía Gutiérrez <127325761+Luciag05@users.noreply.github.com>
2025-03-10 09:55:14 +0100

    v1

---
+++ colocar_barcos.h
@@ -0,0 +1,24 @@
+#ifndef _COLCAR_BARCOS_H_
+#define _COLOCAR_BARCOS_H
+
+#include"MenuP.h"
+
+//cabeceras de funciones y definición de las clases
+
+//Cabecera: void colocar_barcos()
+//Precondición: 
+//Postcondición: crea el tablero Flota de ambos jugadores
+void colocar_barcos();
+
+//Cabecera: void mostrar_info_barcos(bar_vec v_barcos)
+//Precondición: la variable v_barcos no debe ser nula
+//Postcondición: muestra la información de todos los barcos que debe colocar el usuario
+void mostrar_info_barcos(bar_vect v_barcos);
+
+
+
+
+
+
+
+#endif
