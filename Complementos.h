40dc872b6fa2f022bbf94c43b5fbdca858ca575b
Adrián <127325984+RodilloCagable2@users.noreply.github.com>
2025-03-24 23:11:29 +0100

    Complementos v2.2

--- Complementos.h
+++ Complementos.h
@@ -5,30 +5,21 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
-#include <windows.h>
 
-// Estructura para manejar la animación del título
-typedef struct {
-    HANDLE hThread;
-    int activo;
-    int posicion;
-} TituloAnimado;
+void clear ();
 
-void clear();
-void titulo();
-void iniciar_titulo_animado(TituloAnimado* titulo);
-void detener_titulo_animado(TituloAnimado* titulo);
+void titulo ();
 
 //PRECONDICIÓN: Se le pasa una cadena
 //POSCONDICIÓN: Se le cambia el caracter salto de linea por terminador
-void terminador_cad(char*);
+void terminador_cad (char *);
 
 //PRECONDICIÓN: Ninguna
 //POSCONDICIÓN: Devuelve un caracter para respuestas si/no
-void confirmacion(char*);
+void confirmacion (char *);
 
 //PRECONDICIÓN: Ninguna
 //POSCONDICIÓN: Devuelve siempre un número entero
-int input_int();
+int input_int ();
 
 #endif
